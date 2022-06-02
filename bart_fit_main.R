a23663d1b3a95d124f98a4a252cc34f26b155547
weiym97 <weiym97@163.com>
2022-06-02 14:17:14 +0800

    new

--- bart_fit_main.R
+++ bart_fit_main.R
@@ -63,7 +63,7 @@ nWarmup   = floor(nIter/2)
 nThin     = 1
 
 
-modelFile = './BASEBart_18.stan'
+modelFile = './BASEBart_19.stan'
 cat("Estimating", modelFile, "model... \n")
 startTime = Sys.time(); print(startTime)
 cat("Calling", nChains, "simulations in Stan... \n")
@@ -84,4 +84,4 @@ endTime = Sys.time(); print(endTime)
 cat("It took",as.character.Date(endTime - startTime), "\n")
 
 # save the result
-save(fit,file="fit_result/BASEBart_18_MDD_13.Rdata")
+save(fit,file="fit_result/BASEBart_19_MDD_13.Rdata")
