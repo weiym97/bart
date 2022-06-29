477cf857882a7af4ce4333444cc22a5098281e14
weiym97 <weiym97@163.com>
2022-06-29 16:47:16 +0800

    new

--- bart_fit_main.R
+++ bart_fit_main.R
@@ -90,5 +90,5 @@ endTime = Sys.time(); print(endTime)
 cat("It took",as.character.Date(endTime - startTime), "\n")
 
 # save the result
-save(fit,file="fit_result/PTBart_6_MDD_13.Rdata")
+save(fit,file="fit_result/PTBart_6_second_try.Rdata")
 
