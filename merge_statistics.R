1a505e290d7edaf1ff3184fe0a52b4541cca7342
weiym97 <weiym97@163.com>
2022-07-08 10:56:36 +0800

    new

--- merge_statistics.R
+++ merge_statistics.R
@@ -5,7 +5,7 @@ model_name <- args[1]
 data_name <- args[2]
 n_file <- as.integer(args[3])
 
-result <- read.table(paste('fit_result/',model_name,'_',data_name,'_1.txt',sep=''))
+result <- read.table(paste('fit_result/summary',model_name,'_',data_name,'_1.txt',sep=''))
 for (i in 2:n_file){
   new_result <-read.table(paste('fit_result/',model_name,'_',data_name,'_',i,'.txt',sep=''))
   result <- rbind(result,new_result)
