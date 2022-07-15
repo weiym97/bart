cc6eddb91339b77e0c6a0441bb962bd4eb65f9b4
weiym97 <weiym97@163.com>
2022-07-15 11:14:52 +0800

    new

--- plot_recovery.R
+++ plot_recovery.R
@@ -40,7 +40,7 @@ model_name <- args[1]
 data_name <- args[2]
 
 stat_sim <- read.csv(paste('data/simulation/',data_name,'_statistics.csv',sep=''))
-stat_recov <- read.table('fit_result/summary_',model_name,'_',data_name,'.txt',header=T)
+stat_recov <- read.table(paste('fit_result/summary_',model_name,'_',data_name,'.txt'),header=T)
 
 params=return_param(model_name)
 for (i in length(params)){
